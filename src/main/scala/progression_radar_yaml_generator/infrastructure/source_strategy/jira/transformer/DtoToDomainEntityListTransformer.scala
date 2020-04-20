ecc8560fffc5aee1f70155c132c389dbaa80a6f0
Asier Marqués <asiermarques@packlink.com>
2020-04-21 00:33:20 +0200

    DtoToDomainEntityListTransformer refactor

--- src/main/scala/progression_radar_yaml_generator/infrastructure/source_strategy/jira/transformer/DtoToDomainEntityListTransformer.scala
+++ src/main/scala/progression_radar_yaml_generator/infrastructure/source_strategy/jira/transformer/DtoToDomainEntityListTransformer.scala
@@ -14,37 +14,19 @@ object DtoToDomainEntityListTransformer {
   def transform(responseDTO: CategoriesJiraResponseDTO): Seq[Category] =
     processCategoriesKPIs(responseDTO.issues) pipe sortCategoriesByName pipe sortCategoriesKPIsByLevel
 
-  @tailrec
-  private def processCategoriesKPIs(
-    issues: Array[CategoriesJiraIssueDTO],
-    result: Seq[Category] = Seq.empty
-  ): Seq[Category] =
-    if (issues.isEmpty)
-      result
-    else {
-      val issueDTO     = issues.head
-      val categoryName = issueDTO.fields.status.name
-      val kpi          = createKPI(issueDTO)
-      result
-        .find(_.name.equals(categoryName)) match {
-        case Some(category) =>
-          processCategoriesKPIs(
-            issues.tail,
-            result.filter(_.name != categoryName) :+ category.copy(kpis = category.kpis :+ kpi)
-          )
-        case None =>
-          processCategoriesKPIs(
-            issues.tail,
-            result :+ Category(
-              key = categoryName.substring(0, 4).toLowerCase,
-              name = categoryName,
-              description = "",
-              kpis = Seq(kpi)
-            )
-          )
-      }
-
-    }
+  def processCategoriesKPIs(issues: Array[CategoriesJiraIssueDTO]): Seq[Category] =
+    issues
+      .map(issue => (issue.fields.status.name, createKPI(issue)))
+      .groupMap(_._1)(_._2)
+      .map(kpiMap =>
+        Category(
+          name = kpiMap._1,
+          key = kpiMap._1.substring(0, 4).toLowerCase,
+          description = "",
+          kpis = kpiMap._2.toSeq
+        )
+      )
+      .toSeq
 
   private def createKPI(issueDTO: CategoriesJiraIssueDTO) = KPI(
     summary = issueDTO.fields.summary,
