5333bdd72b4f72e90ddc9f0ec474e59913ccaed0
Asier Marqués <asiermarques@packlink.com>
2020-04-21 01:07:03 +0200

    cucumber config

--- src/test/scala/progression_radar_yaml_generator/CucumberTest.scala
+++ src/test/scala/progression_radar_yaml_generator/CucumberTest.scala
@@ -4,5 +4,5 @@ import io.cucumber.junit.{Cucumber, CucumberOptions}
 import org.junit.runner.RunWith
 
 @RunWith(classOf[Cucumber])
-@CucumberOptions(features = Array("docs/features"), strict = true)
+@CucumberOptions(features = Array("docs/features"), strict = true, monochrome = false, stepNotifications = true)
 class CucumberTest
